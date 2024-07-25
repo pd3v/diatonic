37cbc7caf942e5f2c6dff747d1a8a279217c8ef4
pd3v <paulo.develop@gmail.com>
2024-07-25 17:11:03 +0100

    add flat notes to index containers

--- src/intervals.h
+++ src/intervals.h
@@ -12,8 +12,8 @@
 #include <tuple>
 #include <regex>
 
-const std::basic_regex<char> NOTE_OCT_REGEX = std::regex("[a-g][b|s]?[0-9]");
-const std::basic_regex<char> NOTE_REGEX = std::regex("[a-g][b|s]?");
+const std::basic_regex<char> NOTE_OCT_REGEX = std::regex("[a-g][f|s]?[0-9]");
+const std::basic_regex<char> NOTE_REGEX = std::regex("[a-g][f|s]?");
 const std::basic_regex<char> OCT_REGEX = std::regex("[0-9]");
 
 enum intervalT {t = 0,min2,maj2,min3,maj3,p4,a4,p5,min6,maj6,min7,maj7,oct,min9,maj9,min10,maj10,p11,a11,p12,min13,maj13,min14,maj14};
@@ -33,7 +33,7 @@ const noteT gf = fs;
 const noteT af = gs;
 const noteT bf = as;
 
-const std::map<std::string,uint16_t> noteIdx{{"c",0},{"cs",1},{"d",2},{"ds",3},{"e",4},{"f",5},{"fs",6},{"g",7},{"gs",8},{"a",9},{"as",10},{"b",11}};
-const std::vector<std::pair<uint16_t,std::string>> idxNote{{0,"c"},{1,"cs"},{2,"d"},{3,"ds"},{4,"e"},{5,"f"},{6,"fs"},{7,"g"},{8,"gs"},{9,"a"},{10,"as"},{11,"b"}};
+const std::map<std::string,uint16_t> noteIdx{{"c",0},{"cs",1},{"df",1},{"d",2},{"ds",3},{"ef",3},{"e",4},{"f",5},{"fs",6},{"gf",6},{"g",7},{"gs",8},{"af",8},{"a",9},{"as",10},{"bf",10},{"b",11}};
+const std::vector<std::pair<uint16_t,std::string>> idxNote{{0,"c"},{1,"cs"},{1,"df"},{2,"d"},{3,"ds"},{3,"ef"},{4,"e"},{5,"f"},{6,"fs"},{6,"gf"},{7,"g"},{8,"gs"},{8,"af"},{9,"a"},{10,"as"},{10,"bf"},{11,"b"}};
 
 std::pair<std::string,uint8_t> strToKeyAndOct(std::string keyAndOct);
